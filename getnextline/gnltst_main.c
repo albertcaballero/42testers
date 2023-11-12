d897dbecd4f508e781228275f5718449b8f53375
AdriaPriego <adripg12@gmail.com>
2023-11-12 18:56:49 +0100

    GNL TESTER DONE

--- getnextline/gnltst_main.c
+++ getnextline/gnltst_main.c
@@ -4,18 +4,18 @@
 
 int	main(int ac, char **av){
 	int fd = open(av[1], O_RDONLY);
-	// int	outfile = open("outfile.txt", O_RDWR | O_CREAT | O_APPEND, 0644);
 	char *line;
 
 	if (ac == 3){
 		if (av[2][0] == '1' && av[2][1] == '2')
 			fd = 234567891;
 	}
+	line = get_next_line(fd);
 	while (line)
 	{
-		line = get_next_line(fd);
-		dprintf(1, "%s", line);
+		printf("%s", line);
 		free (line);
+		line = get_next_line(fd);
 	}
-	// dprintf(1, "\n===========EOF===========\n\n\n");
+	close (fd);
 }
--- getnextline/test_gnl.sh
+++ getnextline/test_gnl.sh
@@ -6,66 +6,501 @@ PURPLE="\033[38;5;93m"
 PINK="\033[38;5;219m"
 NC="\033[0m"
 
-gcc -Wall -Wextra -Werror gnltst_main.c get_next_line.c get_next_line_utils.c
-rm outfile.txt
 cat /dev/urandom | head -1 > randomfile.txt
 cat /dev/urandom | head -10 > randomfilebig.txt
-file="secretfile.txt"
 testnbr=1
+qttError=0
 printf "\n🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲\n"
 printf "$GREEN\n\t🌟=======ALCABALL GNL mini-TESTER========🌟\n$NC"
 printf "$PINK\nIf there's any issue or something to improve, pls tell me!! :)\n\
 	(I'm always at the campus and I'm 24/7 on Slack)\n"
 printf "\n🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲\n"
 
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
-./a.out $file
+#============================================================================================================#
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(dev/random)\n$NC"
-./a.out randomfile.txt
+printf "$PINK\n==============TEST WITHOUT BUFFER_SIZE==============\n$NC"
+gcc -Wall -Wextra -Werror gnltst_main.c get_next_line.c get_next_line_utils.c -o test.out 2> error
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(dev/random big)\n$NC"
-./a.out randomfilebig.txt
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+else
+	printf "$RED KO Check the subject.... 😑\n$NC"
+	printf "$RED Compiling error\n"
+	cat error
+	rm error test.out
+	exit 42
+fi
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(nonexisting file)\n$NC"
-./a.out "NOSUCHFILE"
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
+file="secretfile.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(no file permissions)\n$NC"
-chmod a-r $file
-./a.out $file
-chmod a+r $file
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	./test.out $file 2>> error
+fi
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(big buffer size)\n$NC"
-cc main.c get_next_line.c get_next_line_utils.c -D BUFFER_SIZE=9223
-./a.out $file
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC"
+file="secret.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(small buffer size)\n$NC"
-cc main.c get_next_line.c get_next_line_utils.c -D BUFFER_SIZE=1
-./a.out $file
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(buffer size is 0)\n$NC"
-cc main.c get_next_line.c get_next_line_utils.c -D BUFFER_SIZE=0
-./a.out $file
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC"
+file="randomfile.txt" 
+timeout 10s ./test.out $file > output 2>> error
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(negative buffer size)\n$NC"
-cc main.c get_next_line.c get_next_line_utils.c -D BUFFER_SIZE=-727
-./a.out $file
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
 
-testnbr=$(($testnbr + 1))
-printf "\n$BLUE====TEST $testnbr ====\n$YELLOW(megabig fd)\n$NC"
-./a.out $file "12"
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
 
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC"
+file="randomfilebig.txt"
+./test.out $file > output 2>> test_error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
+
+#============================================================================================================#
+
+testnbr=0
+printf "$PINK\n==============TEST WITH BAD BUFFER_SIZE==============\n$NC"
+printf "$PINK\nBUFFER_SIZE -1\n$NC"
+
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=-1 gnltst_main.c get_next_line.c get_next_line_utils.c -o test.out 2> error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+else
+	printf "$RED KO Check the subject.... 😑\n$NC"
+	printf "$RED Compiling error\n"
+	cat error
+	rm error test.out test_error
+	exit 42
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
+file="secretfile.txt"
+timeout 10s ./test.out $file > output 2>> error
+
+if [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	printf "$GREEN[OK]\n$NC"
+fi
+
+printf "$PINK\nBUFFER_SIZE -100\n$NC"
+
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=-100 gnltst_main.c get_next_line.c get_next_line_utils.c -o test.out 2> error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+else
+	printf "$RED KO Check the subject.... 😑\n$NC"
+	printf "$RED Compiling error\n"
+	cat error
+	rm error test.out test_error
+	exit 42
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
+file="secretfile.txt"
+timeout 10s ./test.out $file > output 2>> error
+
+if [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	printf "$GREEN[OK]\n$NC"
+fi
+
+printf "$PINK\nBUFFER_SIZE 0\n$NC"
+
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=0 gnltst_main.c get_next_line.c get_next_line_utils.c -o test.out 2> error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+else
+	printf "$RED KO Check the subject.... 😑\n$NC"
+	printf "$RED Compiling error\n"
+	cat error
+	rm error test.out test_error
+	exit 42
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
+file="secretfile.txt"
+timeout 10s ./test.out $file > output 2>> error
+
+if [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	printf "$GREEN[OK]\n$NC"
+fi
+
+#============================================================================================================#
+
+testnbr=0
+printf "$PINK\n==============TEST WITH BUFFER_SIZE==============\n$NC"
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=1 gnltst_main.c get_next_line.c get_next_line_utils.c -o test.out 2> error
+
+printf "$PINK\nBUFFER_SIZE 1\n$NC"
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+else
+	printf "$RED KO Check the subject.... 😑\n$NC"
+	printf "$RED Compiling error\n"
+	cat error
+	rm error test.out
+	exit 42
+fi
+
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
+file="secretfile.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC"
+file="secret.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC"
+file="randomfile.txt" 
+timeout 10s ./test.out $file > output 2>> error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC"
+file="randomfilebig.txt"
+./test.out $file > output 2>> test_error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
+
+gcc -Wall -Wextra -Werror -D BUFFER_SIZE=1000 gnltst_main.c get_next_line.c get_next_line_utils.c -o test.out 2> error
+testnbr=0
+printf "$PINK\nBUFFER_SIZE 1000\n$NC"
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+else
+	printf "$RED KO Check the subject.... 😑\n$NC"
+	printf "$RED Compiling error\n"
+	cat error
+	rm error test.out
+	exit 42
+fi
+
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
+file="secretfile.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC"
+file="secret.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC"
+file="randomfile.txt" 
+timeout 10s ./test.out $file > output 2>> error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC"
+file="randomfilebig.txt"
+./test.out $file > output 2>> test_error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
+
+printf "$PINK\nBUFFER_SIZE 100\n$NC"
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+else
+	printf "$RED KO Check the subject.... 😑\n$NC"
+	printf "$RED Compiling error\n"
+	cat error
+	rm error test.out
+	exit 42
+fi
+
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC"
+file="secretfile.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC"
+file="secret.txt"
+timeout 10s ./test.out $file > output 2>> error
+diff output $file
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Empty file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC"
+file="randomfile.txt" 
+timeout 10s ./test.out $file > output 2>> error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
+
+((testnbr++))
+printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC"
+file="randomfilebig.txt"
+./test.out $file > output 2>> test_error
+
+if [ $? -eq 0 ]; then
+	printf "$GREEN[OK]\n$NC"
+elif [ $? -eq 124 ]; then
+	((qttError++))
+	printf "$PURPLE[TIMEOUT]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(normal test)\n$NC" >> error
+	printf "\nTimeout ¿? test better your code"
+else
+	((qttError++))
+	printf "$RED[KO]\n$NC"
+	printf "\n$BLUE==== TEST $testnbr ====\n$YELLOW(Random big file)\n$NC" >> error
+	./test.out $file 2>> error
+fi
+
+printf "In this case the diff isn't important. Focus in leaking and segfault\n"
+
+#============================================================================================================#
+
+#                                           YOUR OWN TEST 							 						 #
+
+#============================================================================================================#
+
+if [ $qttError -gt 0 ]; then
+	printf "\n$RED========TEST FAILED, GOOD TRY!========\n"
+	cat error
+	rm error test_error output test.out
+	exit 42;
+fi
+rm error test_error output test.out
 printf "$NC\n🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲\n"
 printf "\n\t$GREEN 🌟========TESTS COMPLETED, GOOD LUCK!========🌟\n$NC"
 printf "$PINK By Albert Caballero\n Intra: Alcaball\n Github: https://github.com/albertcaballero\n$NC"
 printf "$NC\n🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲🔳🔲\n"
-
-#can be improved using dprintf to tmp files and using cat -e of such files :)
